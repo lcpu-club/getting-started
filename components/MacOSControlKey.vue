4cb008cdfa85c7a0d5522b75a7e2539d08c100dd
Jason Dai <djdjz7@icloud.com>
2024-11-18 08:58:35 +0800

    doc: added macos shortcuts (#28)

---
+++ components/MacOSControlKey.vue
@@ -0,0 +1,14 @@
+<template>
+  <svg width="39" height="48" viewBox="0 0 39 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M2 21.5L19.5 4L37 21.5" stroke="currentColor" stroke-width="5" />
+  </svg>
+</template>
+
+<style scoped>
+svg {
+  display: inline;
+  height: 0.8em;
+  width: 0.8em;
+  vertical-align: baseline;
+}
+</style>
---
+++ components/MacOSShiftKey.vue
@@ -0,0 +1,18 @@
+<template>
+  <svg width="56" height="59" viewBox="0 0 56 59" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M7 23.5L28 3.5L49 23.5H38.5V56.5H17.5V23.5H7Z"
+      stroke="currentColor"
+      stroke-width="5"
+    />
+  </svg>
+</template>
+
+<style scoped>
+svg {
+  display: inline;
+  height: 0.8em;
+  width: 0.8em;
+  vertical-align: baseline;
+}
+</style>
